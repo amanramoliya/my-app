7e878c672ad313c10ca7ff9f747ee0dd742fa191
unknown <amanramoliya88@gmail.com>
2023-01-30 11:25:30 +0530

    ENH:Created pokemon form and created basic ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private fb: FormBuilder) {
+    this.pokemonForm = fb.group({});
+  }
+
   title = 'my-app';
+  pokemonForm: FormGroup;
+
+  ngOnInit() {
+    this.pokemonForm = this.fb.group({
+      name: this.fb.control(''),
+      speciality: this.fb.control(''),
+      img: this.fb.control(''),
+    });
+  }
 }
